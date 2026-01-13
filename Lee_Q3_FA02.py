5846e87d9ca67dd6b2d72c998c3f98c18c30b476
psalmlee-amats <psalm.lee@smc.pshs.edu.ph>
2026-01-13 23:37:37 +0800

    Implement daily step count tracking for friends
    Added a script to track daily step counts for friends over a week.

--- Lee_Q3_FA02.py
+++ Lee_Q3_FA02.py
@@ -1,1 +1,22 @@
+CS2_Rosal_Lee_2DArray_Act1.py
 
+1. Scenario: Daily step count with my BFFs for 5 days
+
+2.
+Rows represent my friends
+Columns represents the days from monday to friday
+
+3. steps = [
+    [5000, 6200, 7000, 6500, 7200],  
+    [4500, 5800, 6000, 6100, 6900],  
+    [7000, 7500, 8000, 7800, 8200]   
+]
+print(steps[1][2])
+steps[0][0] = 5300
+
+
+average_friend3 = sum(steps[2]) / len(steps[2])
+print(average_friend3)
+
+# Reflection:
+I chose daily health since I wanted to include my friends inside my code. However, I didn't choose that just because of my friends, but also because for health and fitness. This will help us track our steps, which we can use to motivate ourselves to keep pushing forward.
